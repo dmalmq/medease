71489863c5af86522224a1dbf3c73524f611e7cb
Markus Nowak <markusnowak99@gmail.com>
2023-05-16 11:24:28 +0900

    booking model working. Also changed bookings value status from boolean to enum

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  enum :status, [ :pending, :confirmed, :rejected ]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_15_092103) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_16_021009) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,9 +19,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_15_092103) do
     t.bigint "user_id", null: false
     t.date "start_date"
     t.date "end_date"
-    t.boolean "status", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["room_id"], name: "index_bookings_on_room_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
